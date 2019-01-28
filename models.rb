54d80091ac94125c4362db33531779a6f1ffc01c
Wonjun <wonjune12@hotmail.com>
2019-01-28 16:13:45 -0500

    fixed model, fixed bundle version

--- models.rb
+++ models.rb
@@ -5,8 +5,8 @@ set :database, 'postgresql:blogs'
 
 ActiveRecord::Base.establish_connection(
   adapter: 'postgresql',
-  host: 'localhost',
   database: 'blogs'
+  
 )
 
 class User < ActiveRecord::Base
@@ -18,34 +18,5 @@ class Post < ActiveRecord::Base
 end
 
 
-
-
-class CreateUsersTable < ActiveRecord::Migration[5.0]
-  def change
-    create_table :users do |user|
-      user.string :username
-      user.string :password
-      user.string :name
-      user.string :email
-      user.string :dob
-      user.datetime :created_timestamp
-      user.references :post
-    end
-  end
-end
-
-class CreatePostsTable < ActiveRecord::Migration[5.0]
-  def change
-    create_table :posts do |post|
-      post.string :username
-      post.string :subject
-      post.string :content
-      post.datetime :created_timestamp
-      post.datetime :updated_timestamp
-      post.references :user
-    end
-  end
-  
-end
 
 
